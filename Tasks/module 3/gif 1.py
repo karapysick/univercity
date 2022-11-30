7b3c00408a83a66d5bcc20831815d6f717e608aa
Kirill <kirill.yarchenko@mail.ru>
2022-11-30 05:53:12 +0300

    8

---
+++ Tasks/module 3/gif 1.py
@@ -0,0 +1,3 @@
+str1 = input()
+str1_list = str1.split(' ')
+print(len(str1_list))
---
+++ Tasks/module 3/gif 2.py
@@ -0,0 +1,3 @@
+str1 = input('введите текст')
+str1_list = str1.split(' ')
+print(str1_list[1]+ ' ' + str1_list[0])
